e61417190edcb071517f212439380806d6c9bb1a
Dylan <128559794+frielmv@users.noreply.github.com>
2024-10-30 23:19:10 -0700

    Update engine.js

--- engine.js
+++ engine.js
@@ -202,14 +202,13 @@ var Engine = {
 	downtime: 0,
 	save() {
 		return {
-			initialTick: this.initialTick,
 			elapsed: this.elapsed,
 			gameTime: this.gameTime
 		};
 	},
 	loadSave(data) {
-		this.initialTick = data.initialTick;
 		this.elapsed = data.elapsed;
+		this.initialTick = now - this.elapsed;
 		this.gameTime = data.gameTime;
 	},
 	tick() {
